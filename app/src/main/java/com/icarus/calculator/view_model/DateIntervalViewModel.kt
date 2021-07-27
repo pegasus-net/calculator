87ab422fa00c95bb256cb882215adc450c4ba59c
廖强 <1767796806@qq.com>
2021-07-27 18:56:22 +0800

    -

--- app/src/main/java/com/icarus/calculator/view_model/DateIntervalViewModel.kt
+++ app/src/main/java/com/icarus/calculator/view_model/DateIntervalViewModel.kt
@@ -1,5 +1,6 @@
 package com.icarus.calculator.view_model
 
+import a.icarus.utils.Logger
 import android.content.Context
 import androidx.databinding.ObservableField
 import com.icarus.calculator.view.DatePickerDialog
@@ -24,18 +25,18 @@ class DateIntervalViewModel {
 
     init {
         day.set("0天")
-        month.set("0月")
-        year.set("0年")
-        hour.set("0小时")
+        month.set("0月0天")
+        year.set("0年0月0天")
+        hour.set("0小时0分钟")
         minute.set("0分钟")
-        week.set("0周")
+        week.set("0周0天")
         workDay.set("0天")
-        start.set("请输入开始时间")
-        end.set("请输入结束时间")
+        start.set("请选择开始时间")
+        end.set("请选择结束时间")
     }
 
     fun showDialog(context: Context, which: Int) {
-        DatePickerDialog(context).setConfirmListener { calendar, year, month, day, hour, minute ->
+        DatePickerDialog(context).setConfirmListener { calendar, _, _, _, _, _ ->
             when (which) {
                 0 -> {
                     startCalendar = calendar
@@ -46,6 +47,15 @@ class DateIntervalViewModel {
                     end.set(dateFormat.format(calendar.time))
                 }
             }
+            calculateInterval()
         }.show()
+    }
+
+    private fun calculateInterval() {
+        val startCalendar = this.startCalendar
+        val endCalendar = this.endCalendar
+        if (startCalendar != null && endCalendar != null) {
+            Logger.t("Calculate")
+        }
     }
 }
